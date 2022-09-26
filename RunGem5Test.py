7f883389446404961a80859e74d708583459f89f
suchandler96 <zj20140160@163.com>
2022-09-26 16:12:22 +0800

    all non-read-only variables now start from 0x9, all read-only ones start from 0x8; fix RunGem5Test

--- RunGem5Test.py
+++ RunGem5Test.py
@@ -1,12 +1,14 @@
 import os
 import sys
 from multiprocessing import Pool
+import random
+import time
 
 
 def run_test(i, tests):
-    print("running test %d of %d" % i + 1, len(tests))
+    print("running test %d of %d" % (i + 1, len(tests)))
     os.system(tests[i])
-    print("finish test %d of %d" % i + 1, len(tests))
+    print("finish test %d of %d" % (i + 1, len(tests)))
 
 
 if __name__ == '__main__':
@@ -16,21 +18,25 @@ if __name__ == '__main__':
     tests = []
     for line in lines:
         if "gem5.opt" in line:
-            tests.append(line)
+            tests.append(line.strip())
 
     os.chdir("/home/lactose/gem5-rtl/")
 
-    for test in tests:
-        words = test.split()
-        idx_of_d = words.index("-d")
-        os.mkdir(words[idx_of_d + 1])
-
-        if "tee" not in test:
-            test += " | tee " + words[idx_of_d + 1] + "/system.log"
-
-    pool = Pool(processes=12)
     for i in range(len(tests)):
-        pool.apply_async(run_test, (i, tests))
+        words = tests[i].split()
+        idx_of_d = words.index("-d")
+        if not os.path.exists(words[idx_of_d + 1]):
+            os.mkdir(words[idx_of_d + 1])
+        if "tee" not in tests[i]:
+            tests[i] += " | tee " + words[idx_of_d + 1] + "/system.log"
+
+    # print(tests)
+    pool = Pool(12)
+    print(len(tests))
+    for i in range(len(tests)):
+        pool.apply_async(run_test, args=(i, tests))
+
+    print("waiting...")
 
     pool.close()
     pool.join()
